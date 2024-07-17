39ff1a9331b0b38323f3a2c0fad88b546841e768
Donovanlkw <99745649+Donovanlkw@users.noreply.github.com>
2024-07-17 14:08:21 +0100

    Update Ini.ps1

--- Ini.ps1
+++ Ini.ps1
@@ -1,23 +1,9 @@
-Get-WindowsCapability -Online | Where-Object Name -like ‘OpenSSH*’
-Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Start-Service sshd
-Set-Service -Name sshd -StartupType ‘Automatic’
-Below command helps setting the firewall for port 22.
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+winrm quickconfig -transport:https
 
-if (!(Get-NetFirewallRule -Name “OpenSSH-Server-In-TCP” -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+winrm enumerate winrm/config/Listener
+winrm get winrm/config/Service
+winrm get winrm/config/Winrs
 
-Write-Output “Firewall Rule ‘OpenSSH-Server-In-TCP’ does not exist, creating it…”
-
-New-NetFirewallRule -Name ‘OpenSSH-Server-In-TCP’ -DisplayName ‘OpenSSH Server (sshd)’ -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
-
-} else {
-
-Write-Output “Firewall rule ‘OpenSSH-Server-In-TCP’ has been created and exists.”
-
-}
-
-
-
-
-### --- https://medium.com/@gopinath.rs/ansible-to-manage-windows-servers-using-openssh-port-22-8921e8db680c#
+winrs -r:http://192.168.1.123:5985/wsman -u:administrator -p:Password1 ipconfig
+winrs -r:https://192.168.1.123:5985/wsman -u:administrator -p:Password1 ipconfig
